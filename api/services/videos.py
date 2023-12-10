b2aef409c431d7d58e85089a2268943eb710d59a
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-12-10 17:02:17 -0500

    fix: update videos service

--- api/services/videos.py
+++ api/services/videos.py
@@ -2,11 +2,14 @@ import uuid
 from fastapi import UploadFile, status
 from api.common.exceptions import AppException
 
-from api.common.utils.file import get_file_extension, remove_file, write_file
-from api.common.utils.os import get_path
+from api.common.utils.file import check_file, get_file_extension, remove_file, write_file
+from api.common.utils.os import make_dirs, join_path
+from api.common.persistence import LocalObjectStore
 from api.common.utils.video import convert_video
 from config import AppConfig
 
+
+videos_db = LocalObjectStore(AppConfig.Videos.DB_PATH)
 
 def upload_video(video: UploadFile, skip_conversion: bool) -> str:
     # Validate video extension
@@ -17,22 +20,20 @@ def upload_video(video: UploadFile, skip_conversion: bool) -> str:
     
     # Save video
     file_name = uuid.uuid4()
-    src_file_path = get_path(
-        f"{AppConfig.STORAGE_PATH}/temp", 
-        f"{file_name}{src_video_extension}",
-        ignore_exists=True,
-        create_dir=True
+    make_dirs(AppConfig.Videos.TEMP_PATH)
+    src_file_path = join_path(
+        AppConfig.Videos.TEMP_PATH, 
+        f"{file_name}{src_video_extension}"
     )
     video_content = video.file.read()
     write_file(src_file_path, video_content, binary=True)
 
     # Convert video
     dst_video_extension = ".mp4"
-    dst_file_path = get_path(
-        f"{AppConfig.STORAGE_PATH}", 
-        f"{file_name}{dst_video_extension}",
-        ignore_exists=True,
-        create_dir=True
+    make_dirs(AppConfig.Videos.STORAGE_PATH)
+    dst_file_path = join_path(
+        AppConfig.Videos.STORAGE_PATH, 
+        f"{file_name}{dst_video_extension}"
     )
     convert_video(src_file_path, dst_file_path, quiet=AppConfig.IS_DEV)
 
@@ -43,5 +44,9 @@ def upload_video(video: UploadFile, skip_conversion: bool) -> str:
 
 
 def delete_video(file_id: str):
-    file_path = get_path(f"{AppConfig.STORAGE_PATH}", f"{file_id}")
+    file_path = check_file(f"{AppConfig.Videos.STORAGE_PATH}", f"{file_id}")
     remove_file(file_path)
+
+
+def clear_videos():
+    videos_db.clear()
